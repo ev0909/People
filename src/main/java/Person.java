7987f4aa0fe9e662c9cba042ea2fd613dff8f55b
Evgeny Nikitin <ev0909@rambler.ru>
2024-08-17 19:17:21 +0300

    fhird commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -33,8 +33,8 @@ public class Person {
         return surname;
     }
 
-    public OptionalInt getAge() {
-        return OptionalInt.empty();
+    public int getAge() {
+        return age;
     }
 
     public String getAddress() {
